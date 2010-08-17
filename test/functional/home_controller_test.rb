601a753663c2aa99c8ed20cab5442a6eefc21231
Arfon Smith <arfon.smith@gmail.com>
2010-08-17 11:48:50 +0100

    Adding basic functional test

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "Home Controller" do
+    setup do
+      @controller = HomeController.new
+      @request    = ActionController::TestRequest.new
+      @response   = ActionController::TestResponse.new   
+    end
+    
+    context "Index not logged in" do
+      setup do
+        get :index
+      end
+      
+      should respond_with :success
+      should render_template :index
+    end
   end
 end
