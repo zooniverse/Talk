b17270a78dabb9ffc75e5326bf5cca72bcdf94a4
Robert Simpson <ttfnrob@gmail.com>
2010-08-17 16:40:12 +0100

    Stream of comments working.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -3,6 +3,8 @@ class AssetsController < ApplicationController
   def show
     @asset = Asset.find_by_zooniverse_id(params[:id])
     @tags = ["Tag1", "Tag2", "Tag3"]
+    
+    @comments = @asset.conversation.comments
   end
   
 end
--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,8 @@
 class CollectionsController < ApplicationController
+  
+  def show
+    @collection = Collection.find_by_zooniverse_id(params[:id])
+    @tags = ["Tag1", "Tag2", "Tag3"]
+  end
+  
 end
