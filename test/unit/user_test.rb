82503bc1c24e687925c6fb3dc8aed0ca7b667464
Stuart Lynn <stuart.lynn@astro.ox.ac.uk>
2010-08-17 16:58:56 +0100

    Basic user tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,5 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  context "A user" do 
+    setup do 
+      @user = Factory :user
+    end
+    
+    should "have keys" do
+      [:zooniverse_user_id, :name, :collections,:live_collections,:comments,:messages, :sent_messages].each do |key|
+        assert @message.respond_to?(key)
+      end
+    end
+    
+    should "have correct associations" do 
+      assert @user.associations.keys.include?("comments")
+      assert @user.associations.keys.include?("collections")
+      assert @user.associations.keys.include?("live_collections")
+      assert @user.associations.keys.include?("messages")
+      assert @user.associations.keys.include?("sent_messages")
+    end
+  end
   
 end
