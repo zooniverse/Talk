ec2b9d0db31b700a73f2afb8ce919ebf42b59d40
Arfon Smith <arfon.smith@gmail.com>
2010-08-17 16:16:24 +0100

    a

--- test/unit/board_test.rb
+++ test/unit/board_test.rb
@@ -2,21 +2,6 @@ require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
   context "Board" do
-    setup do
-      %w(science chat help).each do |name|
-        Board.create(:title => name, :description => "The #{name} board")
-      end
-    end
-
-    should "have #science, #chat and #help boards" do
-      assert_not_nil Board.find_by_title('science')
-      assert_not_nil Board.find_by_title('chat')
-      assert_not_nil Board.find_by_title('help')
-      assert Board.find_by_title('board_that_doesnt_exist')
-    end
-  end
-  
-  context "Board" do
     should "have class methods for boards" do
       %w(science chat help).each do |method|
         assert Board.respond_to?(method)
