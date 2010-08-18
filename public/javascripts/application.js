1a42f553ef2e137bd33208a658ae789f86531c4f
Robert Simpson <ttfnrob@gmail.com>
2010-08-18 16:30:19 +0100

    Tabbed Trends and Recents boxes using jQuery. Thanks Will!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+
+	$('#trends-tabs').tabs();
+	$('#recents-tabs').tabs();
+	
+});
