f4c67fc02f1218c77c3a4932610c728432083e73
Stuart Lynn <stuart.lynn@astro.ox.ac.uk>
2010-08-18 15:23:23 +0100

    fixed the tests taggable broke

--- lib/taggable.rb
+++ lib/taggable.rb
@@ -2,21 +2,23 @@ module Taggable
   def self.included(base)
      base.extend ClassMethods
      base.send :include, InstanceMethods
-   end
+  end
   
   module ClassMethods
     def rank_tags (tags, no=8)
-      sorted = tags.sort { |a,b| a[1] <=> b[1]}
-      high = sorted.last[1]
-      low  = sorted.first[1]
-      spread = high-low 
-      gap = (spread*1.0)/(no*1.0)
-      result = {}
-      sorted.each do |a|
-        tag = a[0]
-        score = a[1]
-        bin = ((score-low)/gap).floor
-        result[tag] = bin
+      result={}
+      unless tags.empty?  
+        sorted = tags.sort { |a,b| a[1] <=> b[1]}
+        high = sorted.last[1]
+        low  = sorted.first[1]
+        spread = high-low 
+        gap = (spread*1.0)/(no*1.0)
+        sorted.each do |a|
+          tag = a[0]
+          score = a[1]
+          bin = ((score-low)/gap).floor
+          result[tag] = bin
+        end
       end
     return result
     end
@@ -24,6 +26,28 @@ module Taggable
   end
   
   module InstanceMethods
+    
+    def rank_tags (no=8)
+        result = {}
+        
+        unless this.tags.empty?  
+          tags=self.tags
+          sorted = tags.sort { |a,b| a[1] <=> b[1]}
+          high = sorted.last[1]
+          low  = sorted.first[1]
+          spread = high-low 
+          gap = (spread*1.0)/(no*1.0)
+          sorted.each do |a|
+            tag = a[0]
+            score = a[1]
+            bin = ((score-low)/gap).floor
+            result[tag] = bin
+          end
+        end
+        result 
+      end
+        
+  
   end
   
 end
