1682dc897e1b74d3afe06cf484f5a18a7c0fad7e
Stuart Lynn <stuart.lynn@astro.ox.ac.uk>
2010-08-19 15:57:17 +0100

    adding very simple and ugly pagination

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,22 +1,50 @@
 class BoardsController < ApplicationController
   def science 
+    @per_page = params[:per_page]
+    @page= params[:page]
+    
+    @per_page ||= 10
+    @page ||= 1    
+    
+    @per_page=@per_page.to_i
+    @page=@page.to_i
+    
     @board = Board.find_by_title("science")
     @board_stats = @board.stats
-    @discussions_list = @board.discussions
+    @discussions_list = @board.discussions.paginate :per_page=>@per_page, :page=>@page
+    @number_of_pages = (@board.discussion_ids.count/@per_page).floor 
+    
     render "show"
   end
   
   def chat 
+    @per_page = params[:per_page]
+    @page= params[:page]
+    @per_page ||= 10
+    @page ||= 1
+    @per_page=@per_page.to_i
+    @page=@page.to_i
+    
     @board = Board.find_by_title("chat")
     @board_stats = @board.stats
-    @discussions_list = @board.discussions
+    @discussions_list = @board.discussions.paginate :per_page=>@per_page, :page=>@page
+    @number_of_pages = (@board.discussion_ids.count/@per_page).floor 
     render "show"
   end
   
   def help
+    @per_page = params[:per_page]
+    @page= params[:page]
+    @per_page ||= 10
+    @page ||= 1
+    
+    @per_page=@per_page.to_i
+    @page=@page.to_i
+    
     @board = Board.find_by_title("help")
     @board_stats = @board.stats
-    @discussions_list = @board.discussions
+    @discussions_list = @board.discussions.paginate :per_page=>@per_page, :page=>@page
+    @number_of_pages = (@board.discussion_ids.count/@per_page).floor 
     render "show"
   end
 end
---
+++ app/helpers/pagination_helper.rb
@@ -0,0 +1,16 @@
+module PaginationHelper
+  def page_bar (page_no,no_of_pages)
+    
+    path = request.path
+    s="<ul>"
+    (1..no_of_pages+1).each do |i|
+      if(i==page_no)
+        s<< "<li class='pagination'> <a href=#{path}?page=#{i}> \<#{i}\> </a> </li>" 
+      else
+        s<< "<li class='pagination'> <a href=#{path}?page=#{i}>#{i}</a> </li>" 
+      end
+    end
+    s<<"</ul>"
+    s.html_safe
+  end
+end
