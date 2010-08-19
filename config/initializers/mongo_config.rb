3e0cc711f6a1a144627b6deb1cf7b418d127436b
Michael Parrish <mtparrish@gmail.com>
2010-08-19 13:58:50 +0100

    Better data

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,3 +1,3 @@
-names = {'development' => 'dev', 'production' => 'prod', 'test' => 'test'}
-MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017)#, :logger => Rails.logger)
-MongoMapper.database = "sellers-#{names[Rails.env]}"
+config = YAML.load_file(Rails.root + 'config' + 'mongodb.yml')
+MongoMapper.setup(config, Rails.env, { :logger => nil })
+MongoMapper.handle_passenger_forking
