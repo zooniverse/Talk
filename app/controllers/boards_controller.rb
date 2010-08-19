43e470b1fbbf217e9daea00d5f38c033c1ecb66d
Robert Simpson <ttfnrob@gmail.com>
2010-08-19 14:08:21 +0100

    boards stuff

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,18 +1,21 @@
 class BoardsController < ApplicationController
   def science 
     @board = Board.find_by_title("science")
+    @board_stats = @board.stats
     @discussions_list = @board.discussions
     render "show"
   end
   
   def chat 
     @board = Board.find_by_title("chat")
+    @board_stats = @board.stats
     @discussions_list = @board.discussions
     render "show"
   end
   
   def help
     @board = Board.find_by_title("help")
+    @board_stats = @board.stats
     @discussions_list = @board.discussions
     render "show"
   end
