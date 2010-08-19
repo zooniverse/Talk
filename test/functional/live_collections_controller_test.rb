c827793c5af6e6f027eefb81b129b85bf9fa0990
Arfon Smith <arfon.smith@gmail.com>
2010-08-19 11:08:16 +0100

    Live collections placeholder tests

--- test/functional/live_collections_controller_test.rb
+++ test/functional/live_collections_controller_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class LiveCollectionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "Live Collections Controller" do
+    setup do
+      @controller = LiveCollectionsController.new
+      @request    = ActionController::TestRequest.new
+      @response   = ActionController::TestResponse.new
+    end
+
+    context "When requesting a Live Collection" do
+      setup do
+        @live_collection = Factory :live_collection
+        get :show, { :id => @live_collection.zooniverse_id }
+      end
+
+      should_respond_with :success
+      should_render_template :show
+      
+      should_eventually "Display the live collection zooniverse_id" do
+        assert_select 'h2.collection-name', :text => @live_collection.zooniverse_id
+      end
+      
+      should_eventually "Display the live collection owner name" do
+        assert_select 'h2.collection-owner-name', :text => @live_collection.user.name
+      end 
+    end
   end
 end
