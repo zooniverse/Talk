78ebf2a99925b4bbe328a310f9318a37386a8365
Michael Parrish <mtparrish@gmail.com>
2010-08-19 16:21:43 +0100

    User#messages_with

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User
   many :comments, :foreign_key => :author_id
   many :messages, :foreign_key => :recipient_id
   many :sent_messages, :class_name => "Message", :foreign_key => :sender_id
+  
+  def messages_with(user)
+    sent_by_them = Message.all(:sender_id => user.id, :recipient_id => id)
+    sent_by_me = Message.all(:sender_id => id, :recipient_id => user.id)
+    combined = sent_by_me + sent_by_them
+    combined.sort{ |a, b| b.created_at <=> a.created_at }
+  end
 end
