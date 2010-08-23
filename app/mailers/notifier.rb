b3b97e9425455f29022f4293480cf969dcd40f95
Arfon Smith <arfon.smith@gmail.com>
2010-08-23 13:14:27 +0100

    Adding attachments to emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,11 +3,13 @@ class Notifier < ActionMailer::Base
   
   def notify_banned_user(user)
     @user = user
+    attachments["sad-face.png"] = File.read("#{Rails.root}/public/images/sad-face.png")
     mail(:to => "#{user.name} <#{user.email}>", :subject => "You have been banned")
   end
   
   def notify_redeemed_user(user)
     @user = user
+    attachments["happy-face.png"] = File.read("#{Rails.root}/public/images/happy-face.png")
     mail(:to => "#{user.name} <#{user.email}>", :subject => "Welcome back")
   end
 end
