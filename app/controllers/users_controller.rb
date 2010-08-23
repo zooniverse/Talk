ceb02316a61ec1a1e505a7e33b1205bde4a8e5d3
Arfon Smith <arfon.smith@gmail.com>
2010-08-23 12:14:14 +0100

    Users controller test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_zooniverse_user_id(params[:id])
+    @user = User.find(params[:id])
+    
     if (@user.nil?)
       logger.info("// user is nil")
     end
-        
-    @user = User.all.last
-    
   end
-
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  context "Users Controller" do
+    setup do
+      @controller = UsersController.new
+      @request    = ActionController::TestRequest.new
+      @response   = ActionController::TestResponse.new
+    end
 
+    context "When requesting a User" do
+      setup do
+        @user = Factory :user
+        get :show, { :id => @user.id }
+      end
+
+      should respond_with :success
+      should render_template :show
+      
+      should_eventually "Display the user display name" do
+        assert_select 'h2.user-name', :text => @user.name
+      end
+    end
+  end
 end
