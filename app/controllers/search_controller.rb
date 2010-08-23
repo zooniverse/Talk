7b5ab2fa47c97ab8df147a8ef6d109055a29b494
Stuart Lynn <stuart.lynn@astro.ox.ac.uk>
2010-08-23 17:16:45 +0100

    Searching on assets and discussions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,13 @@ class SearchController < ApplicationController
   
   def index 
     @text=params[:text]
-    @results= Comment.search @text, :from_mongo=>true if(@text)
-    logger.info(params)
-    logger.info(@results)
+    
+    if(@text)
+      @results_comments= Comment.search @text, :from_mongo=>true 
+      @results_discussions= (Comment.search @text, :from_mongo=>true, :collapse=>:discussion_id).collect{|d| Discussion.find(d[:discussion_id])}
+      @results_assets =@results_discussions.collect{|d| d.focus if d.focus_type=="Asset"}.compact!
+    end 
+    
   end
   
 end
