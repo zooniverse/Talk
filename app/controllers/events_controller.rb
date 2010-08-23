220b9a39089f2fce8c8f3df2cb09fe156c113dc5
Arfon Smith <arfon.smith@gmail.com>
2010-08-23 16:21:46 +0100

    Adding JQuery rails helper

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
   before_filter CASClient::Frameworks::Rails::Filter
+  respond_to :js
   
   def report_comment
-    comment = Comment.find(params[:comment_id])
-    @event = comment.events.build( :user => current_zooniverse_user, 
+    @comment = Comment.find(params[:comment_id])
+    @event = @comment.events.build( :user => current_zooniverse_user, 
                                       :title => "Comment reported by #{current_zooniverse_user.name}")
     
     
