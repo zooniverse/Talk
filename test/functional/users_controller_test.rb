ae311773234eb392471ea44377ed7f12f2b863b1
Arfon Smith <arfon.smith@gmail.com>
2010-08-24 12:00:00 +0100

    Checking that you don't get moderation links when not a moderator

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -21,5 +21,43 @@ class UsersControllerTest < ActionController::TestCase
         assert_select 'h2.user-name', :text => @user.name
       end
     end
+    
+    context "When viewing a user profile as a moderator" do
+      setup do
+        moderator_cas_login
+        @viewed_user = Factory :user
+        get :show, { :id => @viewed_user.id }
+      end
+
+      should respond_with :success
+      should render_template :show
+      
+      should "Show the report link" do
+        assert_select "#report-user-link-#{@viewed_user.id}"
+      end
+      
+      should "Show the moderation links" do
+        assert_select "#moderation-links"
+      end
+    end
+    
+    context "When viewing a user profile as a standard user" do
+      setup do
+        standard_cas_login
+        @viewed_user = Factory :user
+        get :show, { :id => @viewed_user.id }
+      end
+
+      should respond_with :success
+      should render_template :show
+      
+      should "Show the report link" do
+        assert_select "#report-user-link-#{@viewed_user.id}"
+      end
+      
+      should "not show the moderation links" do
+        assert_select "#moderation-links", 0
+      end
+    end
   end
 end
