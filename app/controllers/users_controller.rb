46e4d80eac7ed7f75e38e52ab981080d39944775
wmacfarlane <wm@zooniverse.org>
2010-08-27 14:29:10 +0100

    ban and activate added to privilage filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_filter CASClient::Frameworks::Rails::Filter
-  
+  before_filter :require_privileged_user, :only => [:activate, :ban]
+    
   respond_to :js, :only => [:report, :activate, :ban]
   respond_to :html, :only => [:show]
-  
+
   def show
     @user = User.find(params[:id])
     @discussions = Discussion.where(:started_by_id => @user.id).sort(['number_of_comments', -1]) 
@@ -17,38 +18,36 @@ class UsersController < ApplicationController
     if @event.save
       User.moderators.each { |moderator| Notifier.notify_reported_user(@user, moderator, current_zooniverse_user).deliver }
     end
-  end
+  end  
   
-  
-  def ban        
+  def ban      
     @user = User.find(params[:id])
-    @user.state = "banned"  
-            
+    @user.state = "banned"              
     if @user.save
-       Notifier.notify_banned_user(@user)      
-        respond_to do |format|
-          format.js {
-            render :update do |page|
-              page['#moderation-links'].html(render :partial => 'shared/user_state')            
-            end    
-          }
-       end      
-     end   
+       Notifier.notify_banned_user(@user).deliver      
+        respond_with(@user) do |format|
+            format.js { 
+              render :update do |page|              
+                page['#moderation-links'].html(render :partial => 'shared/user_state')                          
+              end
+            }
+        end      
+    end
   end
   
-  def activate        
+  def activate          
     @user = User.find(params[:id])
     @user.state = "active"  
-            
+          
     if @user.save
-        respond_to do |format|
-          format.js {
-            render :update do |page|
-              page['#moderation-links'].html(render :partial => 'shared/user_state')            
-            end    
+      respond_with(@user) do |format|
+          format.js { 
+            render :update do |page|              
+              page['#moderation-links'].html(render :partial => 'shared/user_state')                          
+            end
           }
-       end      
-     end   
+      end
+    end   
   end
-  
+    
 end
