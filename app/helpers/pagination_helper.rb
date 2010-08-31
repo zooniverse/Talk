6ab145e6960dd424b04ac3ce11a86230bd3f2451
Michael Parrish <mtparrish@gmail.com>
2010-08-31 16:53:14 -0500

    Valid pagination html

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,16 +1,16 @@
 module PaginationHelper
-  def page_bar (page_no,no_of_pages)
+  def page_bar(current_page, total_pages)
+    bar = "<ul>"
     
-    path = request.path
-    s="<ul>"
-    (1..no_of_pages).each do |i|
-      if(i==page_no)
-        s<< "<li class='pagination'> <a href=#{path}?page=#{i}> \<#{i}\> </a> </li>" 
-      else
-        s<< "<li class='pagination'> <a href=#{path}?page=#{i}>#{i}</a> </li>" 
-      end
+    1.upto(total_pages) do |page|
+      page_name = page == current_page ? "<#{page}>" : "#{page}"
+      
+      bar += "<li class=\"pagination\">"
+      bar += link_to page_name, "#{request.path}?page=#{page}"
+      bar += "</li>"
     end
-    s<<"</ul>"
-    s.html_safe
+    
+    bar += "</ul>"
+    bar.html_safe
   end
 end
