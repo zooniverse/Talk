e014a1f93611a737aa3e9ba06e7d80de96e7c9b3
Michael Parrish <mtparrish@gmail.com>
2010-09-01 16:45:53 -0500

    General Home functionals

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  context "Home Controller actions when not logged in" do
+  context "A HomeController" do
     setup do
       @controller = HomeController.new
       @request    = ActionController::TestRequest.new
@@ -11,22 +11,40 @@ class HomeControllerTest < ActionController::TestCase
     context "#index not logged in" do
       setup do
         CASClient::Frameworks::Rails::GatewayFilter.stubs(:filter).returns(true)
+        @asset = Factory :asset
+        build_focus_for(@asset)
+        collection_for(@asset)
+        board_discussions_in Board.help
         get :index
       end
       
       should respond_with :success
       should render_template :index
+      
+      should "Show trending" do
+        assert_select "#trending-assets > .recent-asset", 3
+        assert_select "#trending-collections > .recent-item", 1
+        assert_select "#trending-discussions > .recent-item", 5
+        assert_select "#keyword-cloud > li", 3
+      end
+      
+      should "Show recent" do
+        assert_select "#recent-assets > .recent-asset", 3
+        assert_select "#recent-collections > .recent-item", 1
+        assert_select "#recent-discussions > .recent-item", 5
+        assert_select "#recent-comments > .recent-comment", 5
+      end
+      
+      should "Show board discussions" do
+        assert_select ".boards-list", 3
+        assert_select ".boards-list ul > li.name", 6
+      end
+      
+      should "Show about box" do
+        assert_select "#about-box"
+      end
     end
-  end
-  
-  context "Home Controller actions when logged in as standard user" do
-    setup do
-      @controller = HomeController.new
-      @request    = ActionController::TestRequest.new
-      @response   = ActionController::TestResponse.new   
-      standard_cas_login
-    end
-
+    
     context "#cas_test logged in" do
       setup do
         standard_cas_login
