275d5009e76c46adbbdd6f30418169125f3dde0f
Michael Parrish <mtparrish@gmail.com>
2010-09-22 15:39:11 -0500

    Fixing up tag and mention linking in comments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,40 +8,40 @@ var OCT = window.OCT || {};
  * @Namespace Tabs
  */
 OCT.tabs = {
-	trends			: '#trends-tabs',
-	recents			: '#recents-tabs',
+  trends      : '#trends-tabs',
+  recents      : '#recents-tabs',
 
-	// Initialise bindings for tabs
-	init: function () {
-		$(OCT.tabs.trends).tabs();
-		$(OCT.tabs.recents).tabs();
-	}
+  // Initialise bindings for tabs
+  init: function () {
+    $(OCT.tabs.trends).tabs();
+    $(OCT.tabs.recents).tabs();
+  }
 };
 
 /* 
  * @Namespace Collection Viewing
  */
 OCT.collection = {
-	large					: '.collection-large',	
-	thumbnail			: '.collection-thumbnail',
+  large          : '.collection-large',
+  thumbnail      : '.collection-thumbnail',
 
-	// Initialise switching behaviour
-	init: function () {
-		$(OCT.collection.thumbnail).mouseover(function() {
-			$(OCT.collection.large).attr("src", $(this).attr("src"));
-		});
-	}
+  // Initialise switching behaviour
+  init: function () {
+    $(OCT.collection.thumbnail).mouseover(function() {
+      $(OCT.collection.large).attr("src", $(this).attr("src"));
+    });
+  }
 };
 
 OCT.loading = {
-	
-	init: function() {
-		  var toggleLoading = function() { $(".loading").toggle() };
+  
+  init: function() {
+      var toggleLoading = function() { $(".loading").toggle() };
 
-		  $(".show-more")
-				.bind("ajax:loading",  toggleLoading)
-		    .bind("ajax:complete", toggleLoading);		
-	}
+      $(".show-more")
+        .bind("ajax:loading",  toggleLoading)
+        .bind("ajax:complete", toggleLoading);
+  }
 }
 
 
@@ -50,67 +50,67 @@ OCT.loading = {
  */
 OCT.hover = {
 
-		item			: '.short-comment, .comment',
-		toolbar : '.toolbar',
-		
-		init: function () {
-			$(OCT.hover.item).hover(function() {	
-				$(OCT.hover.toolbar, this).css('visibility', 'visible');
-			}, function() {
-				$(OCT.hover.toolbar, this).css('visibility', 'hidden');
-			});		
-		}
+    item      : '.short-comment, .comment',
+    toolbar : '.toolbar',
+    
+    init: function () {
+      $(OCT.hover.item).hover(function() {
+        $(OCT.hover.toolbar, this).css('visibility', 'visible');
+      }, function() {
+        $(OCT.hover.toolbar, this).css('visibility', 'hidden');
+      });
+    }
 };
 
 OCT.textcount = {
-	short_text : '#short-text',
-	short_counter : "#short-counter",
-	short_max : 140,
-	
-	init: function() {
-		$(OCT.textcount.short_text).keyup(function() {
-			var remaining = OCT.textcount.short_max - $(OCT.textcount.short_text).val().length;
-			$(OCT.textcount.short_counter).html(remaining);
-		});
-		
-		$(OCT.textcount.short_counter).html(OCT.textcount.short_max);
-		$(OCT.textcount.short_text).attr("maxlength", OCT.textcount.short_max);
-	}
+  short_text : '#short-text',
+  short_counter : "#short-counter",
+  short_max : 140,
+  
+  init: function() {
+    $(OCT.textcount.short_text).keypress(function() {
+      var remaining = OCT.textcount.short_max - $(OCT.textcount.short_text).val().length;
+      $(OCT.textcount.short_counter).html(remaining);
+    });
+    
+    $(OCT.textcount.short_counter).html(OCT.textcount.short_max);
+    $(OCT.textcount.short_text).attr("maxlength", OCT.textcount.short_max);
+  }
 };
 
 
 // --
 $(document).ready(function(){
-		OCT.tabs.init();
-		OCT.collection.init();
-		OCT.hover.init();
-		OCT.loading.init();
-		OCT.textcount.init();
-		$(".highlight_keywords").keywordHighlight();
+    OCT.tabs.init();
+    OCT.collection.init();
+    OCT.hover.init();
+    OCT.loading.init();
+    OCT.textcount.init();
+    $(".highlight_keywords").keywordHighlight();
 });
 
 
 // Utility
 function clear_input(a){
-	myfield = document.getElementById(arguments[0]);
-	myfield.value = "";
+  myfield = document.getElementById(arguments[0]);
+  myfield.value = "";
 }
 
 function replace_input(a,b){
-	myfield = document.getElementById(arguments[0]);
-	if (myfield.value == "") {
-	 myfield.value = arguments[1];
-	}
+  myfield = document.getElementById(arguments[0]);
+  if (myfield.value == "") {
+   myfield.value = arguments[1];
+  }
 }
 
 function clear_this(element){
-	element.value = "";
+  element.value = "";
 }
 
 function replace_this(element){
-	if (element.value == ""){
-		element.value = "Add a keyword";
-	} 
+  if (element.value == ""){
+    element.value = "Add a keyword";
+  } 
 }
 
 /* 
@@ -118,44 +118,44 @@ function replace_this(element){
  */
 
 function reply_to(comment_id, author){
-	$('#comment-form-title').html('Reply to '+ author)
-	$('#comment_response_to_id').val(comment_id);
-	$('html,body').animate({ scrollTop: $('#new_comment').offset().top }, { duration: 'medium', easing: 'swing'});
+  $('#comment-form-title').html('Reply to '+ author)
+  $('#comment_response_to_id').val(comment_id);
+  $('html,body').animate({ scrollTop: $('#new_comment').offset().top }, { duration: 'medium', easing: 'swing'});
 }
 
 // Collection/live collection form JS
 
 function check_collection_type(){
-	if ($('#collection_kind_id').val() == "Live Collection"){
-		$('#live_collection_form').show();
-	} else if ($('#collection_kind_id').val() == "Collection"){
-		$('#live_collection_form').hide();
-	}
+  if ($('#collection_kind_id').val() == "Live Collection"){
+    $('#live_collection_form').show();
+  } else if ($('#collection_kind_id').val() == "Collection"){
+    $('#live_collection_form').hide();
+  }
 }
 
 function remove_keyword_field(field_id){
-	$('#'+field_id + '_wrapper').remove();
+  $('#'+field_id + '_wrapper').remove();
 }
 
 function add_keyword_field(){
-	var last = get_keyword_count();
-	count = last + 1;
-	$("#keyword_" + last + "_wrapper").after("<div id='keyword_"+count+"_wrapper'><input class='keyword' id='keyword_"+ count + "' name='keyword["+count+"]' size='30' type='text' value='Add a keyword' onfocus='clear_this(this);' onblur='replace_this(this);' /> <a href='#' onclick='add_keyword_field();'><img alt='Add' height='13' src='/images/add.png' width='13' /></a>" +	" <a href='#' onclick=\"remove_keyword_field('keyword_"+count+"');\"><img alt='Cancel' height='13' src='/images/cancel.png' width='13' /></div>");
+  var last = get_keyword_count();
+  count = last + 1;
+  $("#keyword_" + last + "_wrapper").after("<div id='keyword_"+count+"_wrapper'><input class='keyword' id='keyword_"+ count + "' name='keyword["+count+"]' size='30' type='text' value='Add a keyword' onfocus='clear_this(this);' onblur='replace_this(this);' /> <a href='#' onclick='add_keyword_field();'><img alt='Add' height='13' src='/images/add.png' width='13' /></a>" + " <a href='#' onclick=\"remove_keyword_field('keyword_"+count+"');\"><img alt='Cancel' height='13' src='/images/cancel.png' width='13' /></div>");
 }
 
 function get_keyword_count(){
-	return $('.keyword').length;
+  return $('.keyword').length;
 }
 
 function update_live_collection_results(){
-	var keywords = new Array();
-	$('.keyword').each(function(){
-		keywords.push($(this).val());
-	});
-	
-	$.ajax({
-	   type: "POST",
-	   url: "/search/live_collection_results",
-	   data: "keywords="+keywords.join(',')
-	 });
+  var keywords = new Array();
+  $('.keyword').each(function(){
+    keywords.push($(this).val());
+  });
+  
+  $.ajax({
+     type: "POST",
+     url: "/search/live_collection_results",
+     data: "keywords="+keywords.join(',')
+   });
 }
--- public/javascripts/keyword_highlight/jquery.keywordHighlight.js
+++ public/javascripts/keyword_highlight/jquery.keywordHighlight.js
@@ -1,23 +1,17 @@
-
 $.fn.keywordHighlight = function(options) {
   var defaults = {
   
   };
-  // Extend our default options with those provided.
-
   var opts = $.extend({}, $.fn.keywordHighlight.defaults, options);
-
-  // Our plugin implementation code goes here.
-
-	return this.each(function(){
-			$this = $(this);
-			var text = $this.html();
-		
-			
-			var result=text.replace(/(^|\s)#(\w+)/g, "$1<a class='keyword'  href='/search?search=keywords%3A$2'>#$2</a>");
-			
-			$this.html(result);
-			return $this;
-	});	
-	
+  
+  return this.each(function(){
+      $this = $(this);
+      var text = $this.html();
+      var result = text.replace(/#([-\w\d]{3,40})/g, "<a class='keyword' href='/search?search=keywords%3A$1'>#$1</a>");
+      result = result.replace(/(AMZ\w{7})/g, "<a class='keyword' href='/objects/$1'>$1</a>");
+      result = result.replace(/(CMZ\w{7})/g, "<a class='keyword' href='/collections/$1'>$1</a>");
+      result = result.replace(/(DMZ\w{7})/g, "<a class='keyword' href='/discussions/$1'>$1</a>");
+      $this.html(result);
+      return $this;
+  });
 };
