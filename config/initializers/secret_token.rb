9adf1ab2dcc465cc6551ad5bd176f29d98d54d83
Michael Parrish <mtparrish@gmail.com>
2011-04-27 12:08:16 -0500

    Moving secret token to a config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,7 @@
-# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify config/secret_token.yml
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Talk::Application.config.secret_token = 'cqek6bp97njbdff4rzww3zuezs6yri2p54pe57usbx5431n2bs64scgm5tu1eum748sj5pkme6zad3ffvxesl9dlc986gxhzbsv4v9fv7bljvvjaqgzhxk23s9cuonyf'
+Talk::Application.config.secret_token = YAML.load_file("#{ Rails.root }/config/secret_token.yml")['token']
